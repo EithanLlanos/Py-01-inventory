d9d337f5051a943d4613016348f989432c353707
EithanLlanos <eithanllanosb@gmail.com>
2023-07-18 12:40:34 -0500

    Base

---
+++ Py-1-Inventory/main.py
@@ -0,0 +1,4 @@
+# class Inventory
+# Attributes about id, stocks, prices
+# Methods like add item, update item, check item
+# Dictionary to store the items details, key as item id and value is a dictionary containing the items details
